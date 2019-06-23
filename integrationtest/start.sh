5499513d267d48b2742906e24f891cc2e9f8865d
FutureSaiyajin <futuresaiyajin@online.de>
2019-06-23 17:49:41 +0200

    Add Cucumber-based integration test framework
    fixed start-local-webserver.sh

---
+++ integrationtest/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker-compose up --build --scale chrome=2 --scale firefox=2
--- start-local-webserver.sh
+++ start-local-webserver.sh
@@ -6,5 +6,4 @@ docker-compose down
 echo "Generating fresh website build & starting webserver..."
 yarn build && docker-compose up -d --build
 
-
 echo "Webserver is running at 'http://localhost:8080'."
